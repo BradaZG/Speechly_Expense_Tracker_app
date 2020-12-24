89cd8b254092af6d3ac1c8b7101c3219431b4dfd
Domagoj Marusic <bradazg@gmail.com>
2020-12-24 02:14:11 +0100

    First version

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const isIncome = Math.round(Math.random());
+
+const InfoCard = () => {
+  return (
+    <div
+      elevation={3}
+      style={{ textAlign: 'center', padding: '0 10%', fontSize: '0.8rem' }}
+    >
+      Try saying: <br />
+      Add {isIncome ? 'Income ' : 'Expense '}
+      for {isIncome ? '$100 ' : '$50 '}
+      in Category {isIncome ? 'Salary ' : 'Travel '}
+      for {isIncome ? 'Monday ' : 'Thursday '}
+    </div>
+  );
+};
+
+export default InfoCard;
